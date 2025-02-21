ebdad1a4c3713a29c268ba418a389528996eb22c
damien <>
2025-02-21 15:46:20 +0100

    Twitch API impl in a class
    * in twitch.py, reorganize code in a TwitchChatBot class so it can be cleanly closed when the app is closed
    * update in run.py to manage the class

--- run.py
+++ run.py
@@ -2,16 +2,17 @@ import asyncio
 from flask import Flask, render_template, request
 from db import get_db_connection, get_all_notes
 from websocket import socketio
-from twitch import init_twitch_chat
+from twitch import TwitchChatBot
 from config import AUTH_TYPE, AuthType
 
 # Flask application and socketio server creation
 app = Flask(__name__)
 socketio.init_app(app)
+chatbot = TwitchChatBot()
 
 # For Server-side authencation, launch auth process now
 if AUTH_TYPE == AuthType.SERVER:
-    asyncio.run(init_twitch_chat())
+    asyncio.run(chatbot.init_twitch_chat())
 
 # Route for the root page of the app : in case of client-side authentication render login.html, else render directly index without parameters
 @app.route('/')
@@ -25,7 +26,7 @@ def login():
 def index():
     # In case of client-side authentication init twitch chat bot with code in url params and render index
     if AUTH_TYPE == AuthType.CLIENT and "code" in request.args:
-        asyncio.run(init_twitch_chat(request.args["code"]))
+        asyncio.run(chatbot.init_twitch_chat(request.args["code"]))
 
     # Get all existing notes in db and render index.html with them as param
     conn = get_db_connection()
@@ -34,4 +35,5 @@ def index():
 
 # Run the flask app if this file is executed as main
 if __name__ == "__main__":
-    socketio.run(app, debug=True)
+    socketio.run(app, debug=False)
+    asyncio.run(chatbot.close_twitch_chat())
--- twitch.py
+++ twitch.py
@@ -1,51 +1,55 @@
-import asyncio
 from twitchAPI.twitch import Twitch
 from twitchAPI.oauth import UserAuthenticator
+from twitchAPI.chat import Chat, EventData, ChatMessage
 from twitchAPI.type import AuthScope, ChatEvent
 from twitchAPI.helper import first
-from twitchAPI.chat import Chat, EventData, ChatMessage
-from db import add_note
 from config import AUTH_TYPE, AuthType
+from db import add_note
 
 MY_URL = "http://localhost:17563"
-target_channel = ""
-target_scope = [AuthScope.CHAT_READ, AuthScope.CHAT_EDIT]
 
-async def on_ready(ready_event: EventData):
-    print('Bot is ready for work, joining channel ' + target_channel)
-    await ready_event.chat.join_room(target_channel)
+class TwitchChatBot():
 
-async def on_message(msg: ChatMessage):
-    # print(f'in {msg.room.name}, {msg.user.name} said: {msg.text}')
-    if msg.text.startswith("!postit "):
-        print(f"Postit cmd :{msg.text}")
-        new_note = msg.text.removeprefix("!postit ")
-        print(f"New note :{new_note}")
-        add_note(new_note)
+    def __init__(self):
+        self.twitch = Twitch('6q2as8ylmy8g56yp7wvnak1bv7315x', "26w7b3hxdgi6y4zmjg24x30qt1iniq")
+        self.chat = None
+        self.target_scope = [AuthScope.CHAT_READ, AuthScope.CHAT_EDIT]
+        self.target_channel = ""
+    
+    async def init_twitch_chat(self, code=None):
+        # Get token and refresh token from user code, and set authentification with them
+        if AUTH_TYPE == AuthType.SERVER:
+            auth = UserAuthenticator(self.twitch, self.target_scope, force_verify=False)
+            token, refresh_token = await auth.authenticate()
+        else:
+            auth = UserAuthenticator(self.twitch, self.target_scope, url=MY_URL)
+            token, refresh_token = await auth.authenticate(user_token=code)
 
-async def init_twitch_chat(code=None):
-    # Create and init Twitch object instance
-    tw = await Twitch('6q2as8ylmy8g56yp7wvnak1bv7315x', "26w7b3hxdgi6y4zmjg24x30qt1iniq")
-    # Get token and refresh token from user code, and set authentification with them
-    if AUTH_TYPE == AuthType.SERVER:
-        auth = UserAuthenticator(tw, target_scope, force_verify=False)
-        token, refresh_token = await auth.authenticate()
-    else:
-        auth = UserAuthenticator(tw, target_scope, url=MY_URL)
-        token, refresh_token = await auth.authenticate(user_token=code)
+        await self.twitch.set_user_authentication(token, self.target_scope, refresh_token)
 
-    await tw.set_user_authentication(token, target_scope, refresh_token)
+        # Get the user linked to the token : we will acces their chat
+        username = await first(self.twitch.get_users())
+        self.target_channel = username.display_name
 
-    # Get the user linked to the token : we will acces their chat
-    username = await first(tw.get_users())
-    global target_channel
-    target_channel = username.display_name
+        # Create the chatbox instance and start it
+        self.chat = await Chat(self.twitch)
+        self.chat.register_event(ChatEvent.READY, self.on_ready)
+        self.chat.register_event(ChatEvent.MESSAGE, self.on_message)
+        self.chat.start()
 
-    # Create the chatbox instance and start it
-    chat = await Chat(tw)
-    chat.register_event(ChatEvent.READY, on_ready)
-    chat.register_event(ChatEvent.MESSAGE, on_message)
-    chat.start()
+    async def close_twitch_chat(self):
+        if self.chat != None:
+            self.chat.stop()
+        await self.twitch.close()
 
-    # chat.stop()
-    # await tw.close()
+    async def on_ready(self, ready_event: EventData):
+        print('Bot is ready for work, joining channel ' + self.target_channel)
+        await ready_event.chat.join_room(self.target_channel)
+
+    async def on_message(self, msg: ChatMessage):
+        # print(f'in {msg.room.name}, {msg.user.name} said: {msg.text}')
+        if msg.text.startswith("!postit "):
+            print(f"Postit cmd :{msg.text}")
+            new_note = msg.text.removeprefix("!postit ")
+            print(f"New note :{new_note}")
+            add_note(new_note)
