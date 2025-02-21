efe658839e14755b09aa8c1387a4ca131ceed534
damien <>
2025-02-21 15:02:49 +0100

    Manage an option for authentication on server or client side
    * add config.py for global configuration, contain the option for server or client authentication
    * in twitch.py, manage both authentication options : on server side, prompt authentication request, on client side, use given code to request token
    * in run.py init twitch at starts if server side auth instead fo on root route

--- run.py
+++ run.py
@@ -3,20 +3,28 @@ from flask import Flask, render_template, request
 from db import get_db_connection, get_all_notes
 from websocket import socketio
 from twitch import init_twitch_chat
+from config import AUTH_TYPE, AuthType
 
 # Flask application and socketio server creation
 app = Flask(__name__)
 socketio.init_app(app)
 
-# Route for the index page of the app
+# For Server-side authencation, launch auth process now
+if AUTH_TYPE == AuthType.SERVER:
+    asyncio.run(init_twitch_chat())
+
+# Route for the root page of the app : in case of client-side authentication render login.html, else render directly index without parameters
 @app.route('/')
 def login():
-    return render_template('login.html')
+    if AUTH_TYPE == AuthType.CLIENT:
+        return render_template('login.html')
+    else:
+        return index()
 
 @app.route('/index')
 def index():
-    # Init and start twitch chat bot with code in url params
-    if "code" in request.args:
+    # In case of client-side authentication init twitch chat bot with code in url params and render index
+    if AUTH_TYPE == AuthType.CLIENT and "code" in request.args:
         asyncio.run(init_twitch_chat(request.args["code"]))
 
     # Get all existing notes in db and render index.html with them as param
@@ -27,4 +35,3 @@ def index():
 # Run the flask app if this file is executed as main
 if __name__ == "__main__":
     socketio.run(app, debug=True)
-
--- twitch.py
+++ twitch.py
@@ -5,9 +5,11 @@ from twitchAPI.type import AuthScope, ChatEvent
 from twitchAPI.helper import first
 from twitchAPI.chat import Chat, EventData, ChatMessage
 from db import add_note
+from config import AUTH_TYPE, AuthType
 
-MY_URL = "http://localhost:5000/index"
+MY_URL = "http://localhost:17563"
 target_channel = ""
+target_scope = [AuthScope.CHAT_READ, AuthScope.CHAT_EDIT]
 
 async def on_ready(ready_event: EventData):
     print('Bot is ready for work, joining channel ' + target_channel)
@@ -21,20 +23,21 @@ async def on_message(msg: ChatMessage):
         print(f"New note :{new_note}")
         add_note(new_note)
 
-async def init_twitch_chat(code):
+async def init_twitch_chat(code=None):
     # Create and init Twitch object instance
     tw = await Twitch('6q2as8ylmy8g56yp7wvnak1bv7315x', "26w7b3hxdgi6y4zmjg24x30qt1iniq")
-    print("Twitch object : " + tw.app_id)
-
     # Get token and refresh token from user code, and set authentification with them
-    target_scope = [AuthScope.CHAT_READ, AuthScope.CHAT_EDIT]
-    auth = UserAuthenticator(tw, target_scope, url=MY_URL)
-    token, refresh_token = await auth.authenticate(user_token=code)
+    if AUTH_TYPE == AuthType.SERVER:
+        auth = UserAuthenticator(tw, target_scope, force_verify=False)
+        token, refresh_token = await auth.authenticate()
+    else:
+        auth = UserAuthenticator(tw, target_scope, url=MY_URL)
+        token, refresh_token = await auth.authenticate(user_token=code)
+
     await tw.set_user_authentication(token, target_scope, refresh_token)
 
     # Get the user linked to the token : we will acces their chat
     username = await first(tw.get_users())
-    print("Auth user : " + username.display_name )
     global target_channel
     target_channel = username.display_name
 
@@ -43,3 +46,6 @@ async def init_twitch_chat(code):
     chat.register_event(ChatEvent.READY, on_ready)
     chat.register_event(ChatEvent.MESSAGE, on_message)
     chat.start()
+
+    # chat.stop()
+    # await tw.close()
